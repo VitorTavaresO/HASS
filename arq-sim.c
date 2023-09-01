7bad128131b62842f4090bd0815bfbdfbf8b0485
VitorTavaresO <vitortavares.o55@gmail.com>
2023-09-01 20:21:17 -0300

    Struct

--- arq-sim.c
+++ arq-sim.c
@@ -3,24 +3,48 @@
 #include <stdint.h>
 #include <assert.h>
 #include <stdbool.h>
-
 #include "lib.h"
 
-uint16_t memory[64 * 1024];
+#define MEMORY 64 * 1024
+#define REGISTERS 8
 
-const char *get_reg_name_str(uint16_t reg)
+uint16_t memory[MEMORY];
+uint16_t registers[REGISTERS];
+
+struct Cpu
 {
-	static const char *str[] = {
-		"r0",
-		"r1",
-		"r2",
-		"r3",
-		"r4",
-		"r5",
-		"r6",
-		"r7"};
+	uint16_t addr;
+	uint16_t instruction;
+	uint16_t format;
+	uint16_t opcode;
+	uint16_t destiny;
+	uint16_t operator01;
+	uint16_t operator02;
+};
 
-	return str[reg];
+uint16_t search(struct Cpu cpu)
+{
+	return memory[cpu.addr];
+}
+
+void decode(struct Cpu *cpu)
+{
+	cpu->format = extract_bits(cpu->instruction, 15, 1);
+	switch (cpu->format)
+	{
+	case 0:
+		cpu->opcode = extract_bits(cpu->instruction, 9, 6);
+		cpu->destiny = extract_bits(cpu->instruction, 6, 3);
+		cpu->operator01 = extract_bits(cpu->instruction, 3, 3);
+		cpu->operator02 = extract_bits(cpu->instruction, 0, 3);
+		break;
+	case 1:
+		cpu->opcode = extract_bits(cpu->instruction, 13, 2);
+		cpu->destiny = extract_bits(cpu->instruction, 10, 3);
+		cpu->operator01 = extract_bits(cpu->instruction, 0, 10);
+		cpu->operator02 = 0;
+		break;
+	}
 }
 
 uint16_t add(uint16_t a, uint16_t b)
@@ -53,61 +77,57 @@ uint16_t cmp_nequal(uint16_t a, uint16_t b)
 	return (a != b);
 }
 
-uint16_t (*operationsR[])(uint16_t, uint16_t) = {
-	add,
-	sub,
-	mul,
-	divi,
-	cmp_equal,
-	cmp_nequal};
-
-uint16_t formatR(uint16_t registers[])
-{
-	registers[extract_bits(memory[0], 6, 3)] = operationsR[extract_bits(memory[0], 9, 6)](registers[extract_bits(memory[0], 3, 3)], registers[extract_bits(memory[0], 0, 3)]);
-}
-
 uint16_t mov(uint16_t a)
 {
 	return a;
 }
 
-uint16_t (*operationsL[])(uint16_t) = {
-	mov};
-
-uint16_t formatL(uint16_t registers[])
+void execute(struct Cpu *cpu)
 {
-	registers[extract_bits(memory[0], 10, 3)] = operationsL[0](extract_bits(memory[0], 0, 10));
-}
-
-uint16_t (*formats[])(uint16_t registers[]) = {
-	formatR,
-	formatL};
-
-uint16_t defineFormat(uint16_t registers[])
-{
-	formats[extract_bits(memory[0], 15, 1)](registers);
+	switch (cpu->format)
+	{
+	case 0:
+		switch (cpu->opcode)
+		{
+		case 0:
+			registers[cpu->destiny] = add(registers[cpu->operator01], registers[cpu->operator02]);
+			break;
+		case 1:
+			registers[cpu->destiny] = sub(registers[cpu->operator01], registers[cpu->operator02]);
+			break;
+		case 2:
+			registers[cpu->destiny] = mul(registers[cpu->operator01], registers[cpu->operator02]);
+			break;
+		case 3:
+			registers[cpu->destiny] = divi(registers[cpu->operator01], registers[cpu->operator02]);
+			break;
+		case 4:
+			registers[cpu->destiny] = cmp_equal(registers[cpu->operator01], registers[cpu->operator02]);
+			break;
+		case 5:
+			registers[cpu->destiny] = cmp_nequal(registers[cpu->operator01], registers[cpu->operator02]);
+			break;
+		}
+		break;
+	case 1:
+		registers[cpu->destiny] = mov(registers[cpu->operator01]);
+		break;
+	}
 }
 
 int main(int argc, char **argv)
 {
-	// if (argc != 2)
-	//{
-	//	printf("usage: %s <binfile>\n", argv[0]);
-	//	exit(1);
-	// }
-	uint16_t registers[8];
+
 	memory[0] = 0b1000010001100100;
-	defineFormat(registers);
-	memory[0] = 0b1001100000111001;
-	defineFormat(registers);
-	memory[0] = 0b0000000101110001;
-	defineFormat(registers);
-	printf("%d", registers[5]);
+	memory[1] = 0b1001100000111001;
+	memory[2] = 0b0000000101110001;
+	struct Cpu cpu;
+	cpu.addr = 0;
+	cpu.instruction = search(cpu);
+	printf("%d\n", cpu.instruction);
+	decode(&cpu);
+	printf("%d\n%d\n%d\n%d\n%d\n", cpu.format, cpu.opcode, cpu.destiny, cpu.operator01, cpu.operator02);
+	execute(&cpu);
+	printf("%d\n", registers[1]);
 	return 0;
 }
-
-// uint16_t operator02 = extract_bits(memory[0], 0, 3);
-// uint16_t operator01 = extract_bits(memory[0], 3, 3);
-// uint16_t destiny = extract_bits(memory[0], 6, 3);
-// uint16_t opcode = extract_bits(memory[0], 9, 6);
-// uint16_t format = extract_bits(memory[0], 15, 1);
