07d0e61af867565cf11e9544dd0a12fee444f0c2
VitorTavaresO <vitortavares.o55@gmail.com>
2023-09-11 19:44:03 -0300

    Delete TempCodeRunner

--- monocycle/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-#define DEBUG
-
-#ifdef DEBUG
-#define dprint(...) printf(__VA_ARGS__)
-#define dprintln(f, ...) printf(f "\n", __VA_ARGS__)
-#else
-#define dprint(...)
-#define dprintln(f, ...)
-#endif
